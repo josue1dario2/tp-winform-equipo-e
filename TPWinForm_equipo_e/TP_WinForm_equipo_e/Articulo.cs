5d194d96e5f875204aef63106fd840941e7d4941
German Gonzalez <gandresgonzalez@gmail.com>
2026-09-08 00:33:04 -0300

    feat: Crear clase Articulo

--- TPWinForm_equipo_e/TP_WinForm_equipo_e/Articulo.cs
+++ TPWinForm_equipo_e/TP_WinForm_equipo_e/Articulo.cs
@@ -8,5 +8,20 @@ namespace TP_WinForm_equipo_e
 {
     internal class Articulo
     {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+
+        public List<Imagen> Imagenes { get; set; } = new List<Imagen>();
+
+        public override string ToString()
+        {
+            return $"{Codigo} - {Nombre}";
+        }
     }
 }
