6995d3386c6ebc98b2ca5a1ca70dc5620159af69
Josue Solis <josue1dario2solis3@gmail.com>
2026-09-08 02:24:48 +0200

    feat: Crear Clase Articulo

---
+++ TPWinForm_equipo_e/TP_WinForm_equipo_e/Dominio/Articulo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_WinForm_equipo_e.Dominio
+{
+    internal class Articulo
+    {
+    }
+}
