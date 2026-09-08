e4fc1794dc7606f8bf09e05d820e4a4805cacfa8
Josue Solis <josue1dario2solis3@gmail.com>
2026-09-08 04:59:42 +0200

    feat: Crear clase Marca

---
+++ TPWinForm_equipo_e/TP_WinForm_equipo_e/Marca.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_WinForm_equipo_e
+{
+    internal class Marca
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+        public Marca()
+        {
+        }
+
+        public Marca(string descripcion)
+        {
+            Descripcion = descripcion;
+        }
+
+        public override string ToString()
+        {
+            return Descripcion;
+        }
+    }
+}
