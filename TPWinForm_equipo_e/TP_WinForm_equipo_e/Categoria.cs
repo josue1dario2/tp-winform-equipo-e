736e370b121422ed6b1c3e32a67ed9e48c8fac6a
FabricioBordon <fabriciobordon02@gmail.com>
2026-09-08 01:20:51 -0300

    feat: Implementada la clase categoria

---
+++ TPWinForm_equipo_e/TP_WinForm_equipo_e/Categoria.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_WinForm_equipo_e
+{
+    internal class Categoria
+    {
+
+        public int Id { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public Categoria()
+        {
+        }
+
+
+        public Categoria(string Descripcion)
+        {
+            this.Descripcion = Descripcion;
+        }
+
+        public override string ToString()
+        {
+             return Descripcion;
+        }
+
+
+
+    }
+}
