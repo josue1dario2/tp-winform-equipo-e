a67f573dbbcaaf8a91cdfa7e3e1d54cfdf6306ea
FabricioBordon <fabriciobordon02@gmail.com>
2026-09-12 11:12:06 -0300

    feat: implemente categorianegocio y el metodo listar

---
+++ TPWinForm_equipo_e/TP_WinForm_equipo_e/CategoriaNegocio.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_WinForm_equipo_e
+{
+    internal class CategoriaNegocio
+    {
+
+    public List <Categoria> Listar()
+        {
+
+            SqlConnection Conexion = new SqlConnection();
+            try
+            {
+                
+
+                Conexion.ConnectionString = "server=.\\SQLEXPRESS;database=CATALOGO_P3_DB;integrated security=true";
+
+                SqlCommand Comando = new SqlCommand();
+
+                Comando.Connection = Conexion;
+
+                Conexion.Open();
+
+                Comando.CommandText = "SELECT Id,Descripcion FROM CATEGORIAS";
+                
+                SqlDataReader Lector = Comando.ExecuteReader();
+
+                List<Categoria> ListaDeCategorias = new List<Categoria>();
+
+                while(Lector.Read())
+                {
+
+                    Categoria auxiliar= new Categoria();
+
+                    auxiliar.Id = (int)Lector["Id"];
+                    auxiliar.Descripcion = (string)Lector["Descripcion"];
+                    ListaDeCategorias.Add(auxiliar);
+                }
+
+
+                return ListaDeCategorias;
+            }
+            catch
+            {
+                throw;
+            }
+
+            finally
+            {
+                Conexion.Close();
+            }
+        }
+
+    }
+}
