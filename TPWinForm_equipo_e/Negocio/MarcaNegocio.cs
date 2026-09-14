6257b7064f0ad60dbb317da26c0f4d7673429bc8
German Gonzalez <gandresgonzalez@gmail.com>
2026-09-14 13:44:06 -0300

    refactor MarcaNegocios ahora usa clase AccesoDatos

--- TPWinForm_equipo_e/Negocio/MarcaNegocio.cs
+++ TPWinForm_equipo_e/Negocio/MarcaNegocio.cs
@@ -3,152 +3,107 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
 using Dominio;
+using negocio;
 
 namespace Negocio
 {
     internal class MarcaNegocio
     {
         public List<Marca> Listar()
-
         {
-            SqlConnection Conexion = new SqlConnection();
+            AccesoDatos Datos = new AccesoDatos();
+            List<Marca> ListaDeMarcas = new List<Marca>();
             try
             {
+                Datos.setearConsulta("SELECT Id,Descripcion FROM MARCAS");
 
+                Datos.ejecutarLectura();
 
-                Conexion.ConnectionString = "server=.\\SQLEXPRESS;database=CATALOGO_P3_DB;integrated security=true";
-
-
-                SqlCommand Comando = new SqlCommand();
-
-                Comando.CommandType = System.Data.CommandType.Text;
-
-                Comando.Connection = Conexion;
-
-                Conexion.Open();
-
-                Comando.CommandText = "SELECT Id,Descripcion FROM MARCAS";
-
-                SqlDataReader Lector = Comando.ExecuteReader();
-
-                List<Marca> ListaDeMarcas = new List<Marca>();
-                while (Lector.Read())
+                while (Datos.Lector.Read())
                 {
-
                     Marca Auxiliar = new Marca();
 
-                    Auxiliar.Id = (int)Lector["Id"];
-                    Auxiliar.Descripcion = (string)Lector["Descripcion"];
+                    Auxiliar.Id = (int)Datos.Lector["Id"];
+                    Auxiliar.Descripcion = (string)Datos.Lector["Descripcion"];
                     ListaDeMarcas.Add(Auxiliar);
-
-
                 }
 
                 return ListaDeMarcas;
             }
-
             catch
             {
-
                 throw;
             }
             finally
             {
-                Conexion.Close();
+                Datos.cerrarConexion();
             }
-
-
         }
 
 
         public void Agregar(Marca nueva)
         {
+            AccesoDatos Datos = new AccesoDatos();
             try
             {
-                SqlConnection Conexion = new SqlConnection();
+                Datos.setearConsulta("INSERT INTO MARCAS (Descripcion) VALUES (@descripcion);");
 
-                Conexion.ConnectionString = "server=.\\SQLEXPRESS;database=CATALOGO_P3_DB;integrated security=true";
+                Datos.setearParametro("@descripcion", nueva.Descripcion);
 
-                SqlCommand Comando = new SqlCommand();
-
-                Comando.CommandType = System.Data.CommandType.Text;
-
-                Comando.Connection = Conexion;
-
-                Comando.CommandText = "INSERT INTO MARCAS (Descripcion) VALUES (@descripcion);";
-
-                Comando.Parameters.AddWithValue("@descripcion", nueva.Descripcion);
-
-                Conexion.Open();
-
-                Comando.ExecuteNonQuery();
+                Datos.ejecutarAccion();
             }
             catch
             {
                 throw;
             }
-
+            finally
+            {
+                Datos.cerrarConexion();
+            }
         }
 
         public void Modificar(Marca marca)
         {
+            AccesoDatos Datos = new AccesoDatos();
             try
             {
-                SqlConnection Conexion = new SqlConnection();
+                Datos.setearConsulta("UPDATE MARCAS SET Descripcion = @descripcion WHERE Id = @id;");
 
-                Conexion.ConnectionString = "server=.\\SQLEXPRESS;database=CATALOGO_P3_DB;integrated security=true";
+                Datos.setearParametro("@descripcion", marca.Descripcion);
+                Datos.setearParametro("@id", marca.Id);
 
-                SqlCommand Comando = new SqlCommand();
-
-                Comando.CommandType = System.Data.CommandType.Text;
-
-                Comando.Connection = Conexion;
-
-                Comando.CommandText = "UPDATE MARCAS SET Descripcion = @descripcion WHERE Id = @id;";
-
-                Comando.Parameters.AddWithValue("@descripcion", marca.Descripcion);
-                Comando.Parameters.AddWithValue("@id", marca.Id);
-
-                Conexion.Open();
-
-                Comando.ExecuteNonQuery();
+                Datos.ejecutarAccion();
             }
             catch
             {
                 throw;
             }
-
+            finally
+            {
+                Datos.cerrarConexion();
+            }
         }
 
         public void Eliminar(int id)
         {
+            AccesoDatos Datos = new AccesoDatos();
             try
             {
-                SqlConnection Conexion = new SqlConnection();
+                Datos.setearConsulta("DELETE FROM MARCAS WHERE Id = @id;");
 
-                Conexion.ConnectionString = "server=.\\SQLEXPRESS;database=CATALOGO_P3_DB;integrated security=true";
+                Datos.setearParametro("@id", id);
 
-                SqlCommand Comando = new SqlCommand();
-
-                Comando.CommandType = System.Data.CommandType.Text;
-
-                Comando.Connection = Conexion;
-
-                Comando.CommandText = "DELETE FROM MARCAS WHERE Id = @id;";
-
-                Comando.Parameters.AddWithValue("@id", id);
-
-                Conexion.Open();
-
-                Comando.ExecuteNonQuery();
+                Datos.ejecutarAccion();
             }
             catch
             {
                 throw;
             }
-
+            finally
+            {
+                Datos.cerrarConexion();
+            }
         }
     }
 }
