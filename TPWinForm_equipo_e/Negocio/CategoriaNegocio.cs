4b7e0463bbfa783a8d3f6d972842fafe95bede64
Josue Solis <josue1dario2solis3@gmail.com>
2026-09-15 00:05:05 +0200

    feat(negocio): implementar Listar() en ArticuloNegocio con LEFT JOIN y vincular grilla en formulario

--- TPWinForm_equipo_e/Negocio/CategoriaNegocio.cs
+++ TPWinForm_equipo_e/Negocio/CategoriaNegocio.cs
@@ -1,11 +1,11 @@
-﻿using Dominio;
-using negocio;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dominio;
+using negocio;
 
 namespace Negocio
 {
