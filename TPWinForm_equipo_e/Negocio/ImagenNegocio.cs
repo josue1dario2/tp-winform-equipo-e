01f5dc6ac1fd215e0cc7c1faac8d906d26c99327
German Gonzalez <gandresgonzalez@gmail.com>
2026-09-17 00:37:15 -0300

    feat ImagenNegocio

---
+++ TPWinForm_equipo_e/Negocio/ImagenNegocio.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace negocio
+{
+    public class ImagenNegocio
+    {
+        // Recupera todas las imágenes (URLs) asociadas a un artículo
+        public List<Imagen> Listar(int idArticulo)
+        {
+            List<Imagen> imagenes = new List<Imagen>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT Id, IdArticulo, ImagenUrl FROM IMAGENES WHERE IdArticulo = @idArticulo");
+                datos.setearParametro("@idArticulo", idArticulo);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Imagen imagen = new Imagen(
+                        (int)datos.Lector["Id"],
+                        (int)datos.Lector["IdArticulo"],
+                        (string)datos.Lector["ImagenUrl"]
+                    );
+                    imagenes.Add(imagen);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+            return imagenes;
+        }
+
+        // Inserta una nueva URL asociada al Id de un artículo
+        public void Agregar(Imagen imagen)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("INSERT INTO IMAGENES (IdArticulo, ImagenUrl) VALUES (@idArticulo, @imagenUrl)");
+                datos.setearParametro("@idArticulo", imagen.IdArticulo);
+                datos.setearParametro("@imagenUrl", imagen.ImagenUrl);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        // Borra todas las imágenes asociadas a un artículo
+        public void EliminarPorArticulo(int idArticulo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("DELETE FROM IMAGENES WHERE IdArticulo = @idArticulo");
+                datos.setearParametro("@idArticulo", idArticulo);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
